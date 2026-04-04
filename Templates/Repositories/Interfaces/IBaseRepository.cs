1b1775a4a2150cdf3c16f4d5d679ba366e5e7cfc
maajoopaa <dhdhsjgsh@gmail.com>
2026-04-05 00:51:41 +0300

    Implemented base repository template

---
+++ Templates/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using Templates.Models;
+
+namespace Templates.Repositories.Interfaces;
+
+public interface IBaseRepository<TEntity> where TEntity : BaseDbEntityWithId
+{
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+    Task AddRangeAsync(List<TEntity> entities, CancellationToken cancellationToken);
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken);
+    Task UpdateRangeAsync(List<TEntity> entities, CancellationToken cancellationToken);
+    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken);
+    Task DeleteRangeAsync(List<TEntity> entities, CancellationToken cancellationToken);
+    Task<List<TEntity>> GetAsync(Expression<Func<TEntity, bool>>? predicate, CancellationToken cancellationToken);
+    Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+}
