082df3082c538f4c1a56e7f00709ad43f27e111d
maajoopaa <dhdhsjgsh@gmail.com>
2026-04-06 01:51:22 +0300

    Implemented mapping profile

---
+++ WomenForum.Application/Helpers/MappingProfile.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using WomenForum.Domain.Models;
+using WomenForum.Models;
+using WomenForum.Models.Requests;
+
+namespace WomenForum.Helpers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CreateUserRequest, User>();
+        
+        CreateMap<CreateCategoryRequest, Category>();
+        
+        CreateMap<CreateCommunityRequest, Community>();
+        
+        CreateMap<CreateDiscussionThreadRequest, DiscussionThread>();
+        
+        CreateMap<CreatePostRequest, Post>();
+
+        CreateMap<User, UserDto>()
+            .ForMember(x => x.FollowingCount, y => y.MapFrom(z => z.Following.Count))
+            .ForMember(x => x.FollowersCount, y => y.MapFrom(z => z.Followers.Count));
+        
+        CreateMap<Category, CategoryDto>();
+        
+        CreateMap<UserSettings, UserSettingsDto>();
+
+        CreateMap<Comment, CommentDto>();
+        
+        CreateMap<Community, CommunityDto>();
+        
+        CreateMap<CommunityJoinRequest, CommunityJoinRequestDto>();
+        
+        CreateMap<CommunityMember, CommunityMemberDto>();
+        
+        CreateMap<DiscussionThread, DiscussionThreadDto>();
+        
+        CreateMap<Like, LikeDto>();
+
+        CreateMap<Message, MessageDto>()
+            .ForMember(x => x.ReplyCount, y => y.MapFrom(z => z.Replies.Count));
+        
+        CreateMap<Notification, NotificationDto>();
+        
+        CreateMap<Post, PostDto>();
+        
+        CreateMap<Report, ReportDto>();
+        
+        CreateMap<Subscription, SubscriptionDto>();
+    }
+}
