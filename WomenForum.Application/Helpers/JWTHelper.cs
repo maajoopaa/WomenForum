f93747bdc6ce771bf6fe3781a9ba1cfaca77982f
maajoopaa <dhdhsjgsh@gmail.com>
2026-04-06 01:38:57 +0300

    Implemented JWT generation helper

---
+++ WomenForum.Application/Helpers/JWTHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using WomenForum.Domain.Enums;
+
+namespace WomenForum.Helpers;
+
+public class JWTHelper(IConfiguration configuration)
+{
+    public string GenerateToken(Guid userId, string username, Role role)
+    {
+        var jwt = configuration.GetSection("Jwt");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"] ?? ""));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim("id", userId.ToString()),
+            new Claim(JwtRegisteredClaimNames.Sub, username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwt["ExpireMinutes"])),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
---
+++ WomenForum.Application/Helpers/PasswordHasher.cs
@@ -0,0 +1,10 @@
+﻿namespace WomenForum.Helpers;
+
+public class PasswordHasher
+{
+    public static string HashPassword(string password) => 
+        BCrypt.Net.BCrypt.HashPassword(password);
+
+    public static bool VerifyPassword(string hashedPassword, string password) => 
+        BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+}
