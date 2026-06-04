3ee7b80afe0b34cf16337fa1c339302836401ecf
maajoopaa <dhdhsjgsh@gmail.com>
2026-06-04 22:34:48 +0300

    Pushed missed files

---
+++ WomenForum.Database/Migrations/20260604193402_AddIsBannedAndIsReadFlagsInTheTables.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WomenForum.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsBannedAndIsReadFlagsInTheTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRead",
+                table: "Warnings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBanned",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRead",
+                table: "Warnings");
+
+            migrationBuilder.DropColumn(
+                name: "IsBanned",
+                table: "Users");
+        }
+    }
+}
